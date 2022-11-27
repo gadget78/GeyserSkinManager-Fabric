925e00357eb5498124b8b981f2196929905cc363
Mick <35707644+gadget78@users.noreply.github.com>
2022-11-27 02:53:42 +0000

    revert

--- src/main/java/olivermakesco/de/gsm/fabric/FabricSkinEventListener.java
+++ src/main/java/olivermakesco/de/gsm/fabric/FabricSkinEventListener.java
@@ -1,73 +1,32 @@
-package com.github.camotoy.geyserskinmanager.common.platform;
+package olivermakesco.de.gsm.fabric;
 
-import com.github.camotoy.geyserskinmanager.common.*;
+import com.github.camotoy.geyserskinmanager.common.SkinDatabase;
+import com.github.camotoy.geyserskinmanager.common.platform.BedrockSkinUtilityListener;
 import com.github.camotoy.geyserskinmanager.common.skinretriever.BedrockSkinRetriever;
-import com.github.camotoy.geyserskinmanager.common.skinretriever.GeyserSkinRetriever;
+import io.netty.buffer.Unpooled;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 
-import java.nio.file.Path;
-import java.util.Objects;
 import java.util.UUID;
-import java.util.function.Consumer;
 
-/**
- * @param <P> Player class
- * @param <S> Secondary class that is usually used for applying the profile
- */
-public abstract class SkinEventListener<P, S> implements PlatformPlayerUuidSupport<P> {
-    protected final BedrockSkinRetriever skinRetriever;
-    protected final SkinDatabase database;
-    protected final SkinUploader skinUploader = new SkinUploader();
-    protected final Consumer<String> warningLoggingFunction;
+public class FabricBedrockSkinUtilityListener extends BedrockSkinUtilityListener<ServerPlayer> {
+    public MinecraftServer server;
 
-    public SkinEventListener(Path skinDatabaseLocation, Consumer<String> warningLoggingFunction) {
-        this.database = new SkinDatabase(skinDatabaseLocation);
-        this.skinRetriever = new GeyserSkinRetriever();
-        this.warningLoggingFunction = warningLoggingFunction;
+    public FabricBedrockSkinUtilityListener(SkinDatabase database, BedrockSkinRetriever skinRetriever, MinecraftServer server) {
+        super(database, skinRetriever);
+        this.server = server;
     }
 
-    protected void uploadOrRetrieveSkin(P player, RawSkin skin) {
-        PlayerEntry playerEntry = database.getPlayerEntry(getUUID(player));
-
-        if (playerEntry == null) {
-            // Fresh join
-            uploadSkin(skin, player, null,null);
-        } else {
-            // This player has joined before
-            SkinEntry setSkin = null;
-            for (SkinEntry skinEntry : playerEntry.getSkinEntries()) {
-                if (skinEntry.getBedrockSkin().equals(skin.rawData)) {
-                    setSkin = skinEntry;
-                    break;
-                }
-            }
-            if (setSkin == null) {
-                uploadSkin(skin, player, null, playerEntry);
-            } else {
-                // We have the skin, we can go straight to applying it to the player
-                onSuccess(player, null, setSkin);
-            }
-        }
+    @Override
+    public void sendPluginMessage(byte[] payload, ServerPlayer player) {
+        GeyserSkinManager.LOGGER.info("Sending skin data!");
+        ServerPlayNetworking.send(player, FabricSkinEventListener.PacketID, new FriendlyByteBuf(Unpooled.wrappedBuffer(payload)));
     }
 
-    protected void uploadSkin(RawSkin skin, P player, S other, PlayerEntry playerEntry) {
-        skinUploader.uploadSkin(skin).whenComplete((uploadResult, throwable) -> {
-            UUID uuid = getUUID(player);
-            if (!skinUploader.checkResult(warningLoggingFunction, uuid.toString(), uploadResult, throwable)) {
-                return;
-            }
-
-            PlayerEntry playerEntryToSave;
-            playerEntryToSave = Objects.requireNonNullElseGet(playerEntry, () -> new PlayerEntry(uuid));
-            SkinEntry skinEntry = new SkinEntry(skin.rawData, uploadResult.getResponse().get("value").getAsString(),
-                    uploadResult.getResponse().get("signature").getAsString(), false);
-            playerEntryToSave.getSkinEntries().add(skinEntry);
-
-            onSuccess(player, other, skinEntry);
-
-            // Save the information so we don't have to upload skins to Mineskin again
-            database.savePlayerInformation(playerEntryToSave);
-        });
+    @Override
+    public UUID getUUID(ServerPlayer player) {
+        return player.getUUID();
     }
-
-    public abstract void onSuccess(P player, S other, SkinEntry skinEntry);
 }
